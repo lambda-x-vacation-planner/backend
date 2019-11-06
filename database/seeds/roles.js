4f1636717269161bf38f3e3d164e665d87257086
Taylor <wwwaaavvveeerrr@gmail.com>
2019-11-06 02:56:11 -0500

    created seeds for roles

---
+++ database/seeds/roles.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('table_name').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('table_name').insert([
+        {id: 1, title: 'Admin' , description: 'Admin/Owner'},
+        {id: 2, title: 'Host' , description: 'Host of Activities, has privileges to edit higher level things'},
+        {id: 3, title: 'Member' , description: 'A regular user'}
+      ]);
+    });
+};
