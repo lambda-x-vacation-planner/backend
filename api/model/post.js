0e3262826dbbfc6b1b64afc1b28b6c133d08fa67
Taylor <wwwaaavvveeerrr@gmail.com>
2019-12-09 17:59:55 -0500

    fixed errors on post

--- api/model/post.js
+++ api/model/post.js
@@ -10,34 +10,34 @@ module.exports = {
 };
 
 function find() {
-  return db('thread');
+  return db('post');
 };
 
-async function insert(thread) {
-  return db('thread')
-  .insert(thread);
+async function insert(post) {
+  return db('post')
+  .insert(post);
 };
 
 function findById(id) {
-  return db('thread')
+  return db('post')
     .where({ id })
     .first();
 };
 
 function findByUser(user_id) {
-    return db('thread')
+    return db('post')
       .where({ user_id })
       .first();
   };
 
 function findByThreadId(thread_id) {
-    return db('thread')
+    return db('post')
       .where({ thread_id })
       .first();
   };
 
 function remove(id) {
-  return db('thread')
+  return db('post')
     .where({ id })
     .first()
     .del();
